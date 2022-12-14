c2000c9e851678dc3da0e84adc3834249aa52cee
Rafael Junqueira Coutinho <69867710+rafaeljcoutinho@users.noreply.github.com>
2022-12-14 15:21:49 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from socket  import *
 from constCS import * #-
-
-s = socket(AF_INET, SOCK_STREAM) 
-s.bind((HOST, PORT))  #-
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(("172.31.81.153", 5678))  #-
 s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
 while True:                # forever
   data = conn.recv(1024)   # receive data from client
   if not data: break       # stop if client stopped
   print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+  datas = bytes.decode(data)
+  if datas[0] == '1':
+      conn.send(str.encode(bytes.decode(data)+"*"))
+  if datas[0] == '2':
+      conn.send(str.encode("*"+bytes.decode(data)+"*"))
+  if datas[0] == '3':
+      conn.send(str.encode(bytes.decode(data)+"*-*"))
+  if datas[0] == '4':
+      conn.send(str.encode("*_*"+bytes.decode(data)+"*_*"))
+
 conn.close()               # close the connection
